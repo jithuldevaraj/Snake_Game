047bd840dabe84b15accd8368a98f620e1be52a9
Jithu <jithuldevaraj01@gmail.com>
2024-11-18 18:59:49 +0530

    Detect collisions with tail (use slice List )

--- main.py
+++ main.py
@@ -41,45 +41,10 @@ while game_is_on:
         scoreboard.game_over()
 
     #Ditect collision with Tail
-    for segment in snake.segment:
-        if segment == snake.head:
-            pass
-
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segment[1:len(snake.segment)-1]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 screen.exitonclick()
-
-
-
-
